e4098d6cee5cbac7c5282db2635ba934242c8ad9
allydevper <alamaswilmer@gmail.com>
2025-01-26 20:37:00 -0500

    Update EventDetail.vue

--- src/components/EventDetail.vue
+++ src/components/EventDetail.vue
@@ -20,6 +20,8 @@ const allDatesList = reactive<AvailabilityModel[]>([]);
 const commonDatesList = reactive<AvailabilityModel[]>([]);
 const loading = ref(false);
 const participants = ref(0);
+const eventTitle = ref('');
+const isAdmin = ref(false);
 
 let uid = route.params.uid as string;
 let user = localStorage.getItem('userId') as string;
@@ -38,6 +40,9 @@ watchEffect(async () => {
       if (!event) {
         throw new Error('No se encontró el evento');
       }
+
+      eventTitle.value = event.name;
+      isAdmin.value = event.admin_id == user;
 
       const availability = await getAvailabilityByEventId(uid);
       allDatesList.push(...availability);
@@ -178,6 +183,11 @@ const updateParticipantsAndCommonDates = (availabilityList: AvailabilityModel[])
   <div class="flex items-center justify-center h-screen">
     <div
       :class="['max-w-6xl mx-auto p-8 bg-white text-black shadow-md border-4 border-black', { 'opacity-50 cursor-not-allowed': loading }]">
+      <div v-if="isAdmin" class="flex items-center justify-center">
+        <input v-model="eventTitle" type="text"
+          class="uppercase text-center text-2xl font-bold mb-4 border-black focus:outline-none focus:border-black">
+      </div>
+      <h1 v-else class="uppercase text-center text-2xl font-bold mb-4">{{ eventTitle }}</h1>
       <div class="flex items-center justify-between space-x-4 mb-4">
         <vue-tailwind-datepicker v-model="eventDate" :formatter="formatter" :i18n="i18n" :options="options"
           placeholder="Selecciona una fecha" :disable-date="dDate"
@@ -187,9 +197,10 @@ const updateParticipantsAndCommonDates = (availabilityList: AvailabilityModel[])
       </div>
 
       <div class="grid grid-cols-3 gap-4">
-        <div class="col-span-1 bg-black text-white p-4 border-4 border-black min-w-[250px] max-h-[300px] overflow-y-auto">
+        <div
+          class="col-span-1 bg-black text-white p-4 border-4 border-black min-w-[250px] max-h-[300px] overflow-y-auto">
           <h3 v-if="datesList.length > 0" class="text-lg font-bold mb-6">Mis Fechas Propuestas</h3>
-          <h3 v-if="datesList.length == 0" class="text-lg font-bold mb-6">No hay fechas ingresadas</h3>
+          <h3 v-else class="text-lg font-bold mb-6">No hay fechas ingresadas</h3>
           <ul class="space-y-4">
             <li v-for="date in datesList" :key="date.id">
               <div class="flex items-center justify-between">
@@ -216,7 +227,7 @@ const updateParticipantsAndCommonDates = (availabilityList: AvailabilityModel[])
             <h3 v-if="commonDatesList.length > 0" class="text-lg font-bold">Fechas disponibles del {{ currentYear }} de
               {{ participants }} participantes
             </h3>
-            <h3 v-if="commonDatesList.length == 0" class="text-lg font-bold">No hay relacionadas</h3>
+            <h3 v-else class="text-lg font-bold">No hay relacionadas</h3>
             <!-- <button aria-label="Recargar fechas"
               class="text-black-500 hover:text-black-700 flex items-center justify-end ml-auto">
               <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
